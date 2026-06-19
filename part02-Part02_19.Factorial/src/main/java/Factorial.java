01f56e084253f636bf63ea2cbffb458cf5bbb9d2
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-19 19:42:03 -0300

    Factorial

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -6,5 +6,19 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Give a number:");
+        int number = Integer.valueOf(scanner.nextLine());
+
+        int value = 1;
+
+        if (number == 0) {
+            value = 1;
+        }
+
+        for (int i = 1; i <= number; i++) {
+            value *= i;
+        }
+
+        System.out.println("Factorial: " + value);
     }
 }
