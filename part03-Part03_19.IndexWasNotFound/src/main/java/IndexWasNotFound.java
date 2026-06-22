a0e4fbbfd2e1e769e3908e708981024be27a4af1
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-22 15:40:30 -0300

    Index was not found

--- part03-Part03_19.IndexWasNotFound/src/main/java/IndexWasNotFound.java
+++ part03-Part03_19.IndexWasNotFound/src/main/java/IndexWasNotFound.java
@@ -1,5 +1,4 @@
 
-
 import java.util.Scanner;
 
 public class IndexWasNotFound {
@@ -20,6 +19,20 @@ public class IndexWasNotFound {
         int searching = Integer.valueOf(scanner.nextLine());
 
         // Implement the search functionality here
+        int index = 0;
+        boolean found = false;
+
+        while (index < array.length) {
+            if (array[index] == searching) {
+                System.out.println(searching + " is at index " + index + ".");
+                found = true;
+            }
+            index++;
+        }
+
+        if (!found) {
+            System.out.println(searching + " was not found.");
+        }
     }
 
 }
