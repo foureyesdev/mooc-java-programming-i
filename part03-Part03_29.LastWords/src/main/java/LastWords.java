a94e48d99b86bce98cae2f22a3497ec32aeaa7b8
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-22 18:50:50 -0300

    Last words

--- part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -6,6 +6,17 @@ public class LastWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String input = scanner.nextLine();
 
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] splited = input.split(" ");
+            int lastIndex = splited.length - 1;
+            String lastWord = splited[lastIndex];
+            System.out.println(lastWord);
+        }
     }
 }
