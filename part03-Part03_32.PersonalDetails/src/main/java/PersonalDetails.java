d23f55801202cdd3e4e4e80b6b9676ec0abb8c11
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-22 19:51:41 -0300

    Personal details

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -7,6 +7,40 @@ public class PersonalDetails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<Integer> birthYears = new ArrayList<>();
+        String longestName = "";
 
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] splited = input.split(",");
+            String name = splited[0];
+            int birthYear = Integer.valueOf(splited[1]);
+
+            names.add(name);
+            birthYears.add(birthYear);
+
+            for (String str : names) {
+                if (longestName.length() < str.length()) {
+                    longestName = str;
+                }
+            }
+        }
+
+        System.out.println("Longest name: " + longestName);
+
+        int sum = 0;
+        for (Integer year : birthYears) {
+            sum += year;
+        }
+
+        double avg = 1.0 * sum / birthYears.size();
+
+        System.out.println("Average of the birth years: " + avg);
     }
 }
