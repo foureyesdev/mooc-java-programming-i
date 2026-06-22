2c9655d0052efe221620451dc8d933d856737617
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-22 19:39:51 -0300

    Name of the oldest

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NameOfTheOldest {
@@ -6,6 +7,39 @@ public class NameOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int oldestAge = 0;
+        ArrayList<Integer> ages = new ArrayList<>();
 
+        String oldestName = "";
+        ArrayList<String> names = new ArrayList<>();
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] splited = input.split(",");
+
+            int age = Integer.valueOf(splited[1]);
+            String name = splited[0];
+
+            ages.add(age);
+            names.add(name);
+
+            for (int i = 0; i < ages.size(); i++) {
+                int ageValue = ages.get(i);
+
+                if (oldestAge < ageValue) {
+                    oldestAge = ageValue;
+                    oldestName = names.get(i);
+                }
+            }
+        }
+
+        if (!(ages.isEmpty() && names.isEmpty())) {
+            System.out.println("Name of the oldest: " + oldestName);
+        }
     }
 }
