fdca5a794c4f9b5217cd71b31c568917bb9fe7b0
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-22 19:33:03 -0300

    Age of the oldest

--- part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AgeOfTheOldest {
@@ -6,6 +7,31 @@ public class AgeOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<Integer> ages = new ArrayList<>();
+        int oldestAge = 0;
 
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] splited = input.split(",");
+
+            int age = Integer.valueOf(splited[1]);
+
+            ages.add(age);
+
+            for (int a : ages) {
+                if (oldestAge < a) {
+                    oldestAge = a;
+                }
+            }
+        }
+
+        if (!ages.isEmpty()) {
+            System.out.println("Age of the oldest: " + oldestAge);
+        }
     }
 }
