1e0ad13ba24203575742cb06806f34bf3735f805
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-22 18:21:51 -0300

    Line by line

--- part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -6,6 +6,19 @@ public class LineByLine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] splited = input.split(" ");
+
+            for (int i = 0; i < splited.length; i++) {
+                System.out.println(splited[i]);
+            }
+        }
 
     }
 }
