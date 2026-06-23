6fd557fda1d3d9018ae9a702487ecc307493e984
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-23 14:38:31 -0300

    Payment Card (6 parts)

---
+++ part04-Part04_16.PaymentCard/src/main/java/PaymentCard.java
@@ -0,0 +1,33 @@
+public class PaymentCard {
+    private double balance;
+
+    public PaymentCard(double openingBalance) {
+        this.addMoney(openingBalance);
+    }
+
+    public void eatAffordably() {
+        if (this.balance >= 2.60) {
+            this.balance -= 2.60;
+        }
+    }
+
+    public void eatHeartily() {
+        if (this.balance >= 4.60) {
+            this.balance -= 4.60;
+        }
+    }
+
+    public void addMoney(double amount) {
+        if ((amount > 150) || (amount + this.balance >= 150)) {
+            this.balance = 150;
+        }
+
+        if ((amount > 0) && (amount + this.balance < 150)) {
+            this.balance += amount;
+        }
+    }
+
+    public String toString() {
+        return "The card has a balance of " + this.balance + " euros";
+    }
+}
