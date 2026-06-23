33ff401f9707c06bb1956df830902838bdf8b395
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-23 16:13:16 -0300

    Book

--- part04-Part04_20.Books/src/main/java/Main.java
+++ part04-Part04_20.Books/src/main/java/Main.java
@@ -4,9 +4,44 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        
-        // implement here the program that allows the user to enter 
+
+        // implement here the program that allows the user to enter
         // book information and to examine them
 
+        ArrayList<Book> books = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Title: ");
+            String title = scanner.nextLine();
+
+            if (title.equals("")) {
+                break;
+            }
+
+            System.out.print("Pages: ");
+            int pages = Integer.valueOf(scanner.nextLine());
+
+            System.out.print("Publication year: ");
+            int pubYear = Integer.valueOf(scanner.nextLine());
+
+            Book book = new Book(title, pages, pubYear);
+
+            books.add(book);
+        }
+
+        System.out.print("What information will be printed? ");
+        String info = scanner.nextLine();
+
+        for (Book book : books) {
+            if (info.equals("everything")) {
+                System.out.println(book);
+            }
+
+            if (info.equals("name")) {
+                System.out.println(book.getTitle());
+            }
+        }
     }
 }
