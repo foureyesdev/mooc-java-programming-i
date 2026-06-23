3b7c7c103ee7e94dc025d9caea121bb84a3edbba
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-23 09:47:31 -0300

    Your first account

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,8 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+        Account newAccount = new Account("Lorena", 100);
+        newAccount.deposit(20);
+        System.out.println(newAccount);
     }
 }
