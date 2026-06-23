30a3b774c657533909f9badcfe8786124df9cf7b
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-23 15:17:53 -0300

    Personal information

--- part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -10,5 +10,27 @@ public class PersonalInformationCollection {
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("First name:");
+            String firstName = scanner.nextLine();
+
+            if (firstName.equals("")) {
+                break;
+            }
+
+            System.out.println("Last name:");
+            String lastName = scanner.nextLine();
+
+            System.out.println("Identification number:");
+            String idNumber = scanner.nextLine();
+
+            PersonalInformation info = new PersonalInformation(firstName, lastName, idNumber);
+
+            infoCollection.add(info);
+        }
+
+        for (PersonalInformation info : infoCollection) {
+            System.out.println(info.getFirstName() + " " + info.getLastName());
+        }
     }
 }
