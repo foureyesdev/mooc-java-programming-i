5760a21fb84b1f7d546d07986f688c7686cdad4e
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-23 15:42:39 -0300

    Television programs

--- part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -9,5 +9,29 @@ public class Main {
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+
+            if (name.equals("")) {
+                break;
+            }
+
+            System.out.print("Duration: ");
+            int duration = Integer.valueOf(scanner.nextLine());
+
+            TelevisionProgram program = new TelevisionProgram(name, duration);
+
+            programs.add(program);
+        }
+
+        System.out.print("Program's maximum duration? ");
+        int maxDuration = Integer.valueOf(scanner.nextLine());
+
+        for (TelevisionProgram program : programs) {
+            if (program.getDuration() <= maxDuration) {
+                System.out.println(program);
+            }
+        }
     }
 }
