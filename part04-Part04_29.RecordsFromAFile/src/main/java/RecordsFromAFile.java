6b7107747f9ae86d3d97d2ba8792b7a597ef6519
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-24 14:48:52 -0300

    Records From a File

--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -7,5 +7,27 @@ public class RecordsFromAFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Name of the file: ");
+        String file = scanner.nextLine();
+
+        try (Scanner scan = new Scanner(Paths.get(file))) {
+
+            while (scan.hasNextLine()) {
+                String data = scan.nextLine();
+
+                String[] splitedData = data.split(",");
+                String name = splitedData[0];
+                int age = Integer.valueOf(splitedData[1]);
+
+                if (age == 1) {
+                    System.out.println(name + ", age: " + age + " year");
+                } else {
+                    System.out.println(name + ", age: " + age + " years");
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
