160ba9f826549fcff95af1d518a91b2ed2fec94e
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-24 10:43:08 -0300

    Printing a File

--- part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
+++ part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
@@ -6,5 +6,16 @@ public class PrintingAFile {
 
     public static void main(String[] args) {
 
+        try (Scanner scanner = new Scanner(Paths.get("data.txt"))) {
+
+            while (scanner.hasNextLine()) {
+                String data = scanner.nextLine();
+
+                System.out.println(data);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
