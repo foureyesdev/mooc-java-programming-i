a330ffa90da5abb2ce13de630ebe7418bb40ef79
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-24 14:04:15 -0300

    Numbers From a File

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -14,6 +14,21 @@ public class NumbersFromAFile {
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
 
+        int count = 0;
+        try (Scanner scan = new Scanner(Paths.get(file))) {
+
+            while (scan.hasNextLine()) {
+                int number = Integer.valueOf(scan.nextLine());
+
+                if (number >= lowerBound && number <= upperBound) {
+                    count++;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        System.out.println("Numbers: " + count);
     }
 
 }
