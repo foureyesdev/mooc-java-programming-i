95ad7e74bc6ebec4f8622ef37523ce5bec60b3be
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-24 14:09:09 -0300

    Refactor file search logic to count occurrences instead of storing names

--- part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -1,12 +1,10 @@
 
 import java.nio.file.Paths;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class IsItInTheFile {
 
     public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
 
@@ -16,20 +14,23 @@ public class IsItInTheFile {
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
 
+        int count = 0;
         try (Scanner scan = new Scanner(Paths.get(file))) {
             while (scan.hasNextLine()) {
                 String data = scan.nextLine();
-                names.add(data);
-            }
 
-            if (names.contains(searchedFor)) {
-                System.out.println("Found!");
-            } else {
-                System.out.println("Not found.");
+                if (data.contains(searchedFor)) {
+                    count++;
+                }
             }
         } catch (Exception e) {
             System.out.println("Reading the file " + file + " failed.");
         }
 
+        if (count == 0) {
+            System.out.println("Not found.");
+        } else {
+            System.out.println("Found!");
+        }
     }
 }
