b0b180d3984180137d52c3b1afe1925cccb58508
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-24 11:45:25 -0300

    Is it in the file?

--- part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -1,10 +1,13 @@
 
 import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class IsItInTheFile {
 
     public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Name of the file:");
@@ -12,6 +15,21 @@ public class IsItInTheFile {
 
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
+
+        try (Scanner scan = new Scanner(Paths.get(file))) {
+            while (scan.hasNextLine()) {
+                String data = scan.nextLine();
+                names.add(data);
+            }
+
+            if (names.contains(searchedFor)) {
+                System.out.println("Found!");
+            } else {
+                System.out.println("Not found.");
+            }
+        } catch (Exception e) {
+            System.out.println("Reading the file " + file + " failed.");
+        }
 
     }
 }
