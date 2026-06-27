767c85de7257aa671ae09ddfa38458bc0fa6c9d0
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-27 12:23:00 -0300

    One Minute

---
+++ part05-Part05_01.OneMinute/src/main/java/Timer.java
@@ -0,0 +1,23 @@
+
+public class Timer {
+
+    private ClockHand seconds;
+    private ClockHand hundredthsOfASecond;
+
+    public Timer() {
+        this.seconds = new ClockHand(60);
+        this.hundredthsOfASecond = new ClockHand(100);
+    }
+
+    public void advance() {
+        this.hundredthsOfASecond.advance();
+
+        if (this.hundredthsOfASecond.value() == 0) {
+            this.seconds.advance();
+        }
+    }
+
+    public String toString() {
+        return seconds + ":" + hundredthsOfASecond;
+    }
+}
