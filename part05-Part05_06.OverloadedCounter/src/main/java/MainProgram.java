30a5fc22cef2d0ddd0f3d46ef8ee792ebb7677c9
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-29 16:29:44 -0300

    Counter

--- part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
+++ part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
@@ -3,5 +3,11 @@ public class MainProgram {
 
     public static void main(String[] args) {
         // Test your counter here
+        Counter counter = new Counter(1);
+        counter.increase();
+        counter.increase(4);
+        counter.decrease();
+        counter.decrease(2);
+        System.out.println(counter.value());
     }
 }
