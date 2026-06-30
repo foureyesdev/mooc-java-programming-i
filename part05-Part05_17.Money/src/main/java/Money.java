0431d947a9e35c5125804e61ff3cf82045e1cc66
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-30 19:59:48 -0300

    Money

--- part05-Part05_17.Money/src/main/java/Money.java
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -23,6 +23,48 @@ public class Money {
         return this.cents;
     }
 
+    public Money plus(Money addition) {
+        int newEuros = this.euros + addition.euros();
+        int newCents = this.cents + addition.cents();
+
+        if (newCents > 99) {
+            newCents = newCents - 100;
+            newEuros = newEuros + 1;
+        }
+
+        return new Money(newEuros, newCents);
+    }
+
+    public boolean lessThan(Money compared) {
+        boolean isLessThan = false;
+
+        if ((100 * this.euros + this.cents) < (100 * compared.euros() + compared.cents())) {
+
+            isLessThan = true;
+        }
+
+        return isLessThan;
+    }
+
+    public Money minus(Money decreaser) {
+        int NewEuros = this.euros - decreaser.euros();
+        int newCents = this.cents - decreaser.cents();
+
+        if (newCents < 0) {
+            newCents = newCents + 100;
+            NewEuros = NewEuros - 1;
+        }
+
+        if (NewEuros < 0) {
+            NewEuros = 0;
+            newCents = 0;
+        }
+
+        Money newMoney = new Money(NewEuros, newCents);
+
+        return newMoney;
+    }
+
     public String toString() {
         String zero = "";
         if (this.cents < 10) {
