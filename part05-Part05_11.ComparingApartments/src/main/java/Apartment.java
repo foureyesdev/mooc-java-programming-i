542657063d41e12caf08ad27d8f28afee98cabba
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-06-30 13:56:23 -0300

    Comparing apartments (3 parts)

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -3,12 +3,36 @@ public class Apartment {
 
     private int rooms;
     private int squares;
-    private int princePerSquare;
+    private int pricePerSquare;
 
     public Apartment(int rooms, int squares, int pricePerSquare) {
         this.rooms = rooms;
         this.squares = squares;
-        this.princePerSquare = pricePerSquare;
+        this.pricePerSquare = pricePerSquare;
+    }
+
+    public int calculateTotalPrice() {
+        return this.pricePerSquare * this.squares;
+    }
+
+    public int priceDifference(Apartment compared) {
+        int apartmentPrice = this.calculateTotalPrice();
+        int comparedPrice = compared.calculateTotalPrice();
+        int diff = apartmentPrice - comparedPrice;
+
+        if (diff < 0) {
+            return diff * (-1);
+        }
+
+        return diff;
+    }
+
+    public boolean moreExpensiveThan(Apartment compared) {
+        return this.calculateTotalPrice() > compared.calculateTotalPrice();
+    }
+
+    public boolean largerThan(Apartment compared) {
+        return this.squares > compared.squares;
     }
 
 }
