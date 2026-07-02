821a223efa437468c5518dd6daf838b98df2f875
Lorena Facundes <111245669+foureyesdev@users.noreply.github.com>
2026-07-02 11:59:12 -0300

    Longest in collection

--- part06-Part06_06.LongestInCollection/src/main/java/SimpleCollection.java
+++ part06-Part06_06.LongestInCollection/src/main/java/SimpleCollection.java
@@ -19,4 +19,19 @@ public class SimpleCollection {
         return this.elements;
     }
 
+    public String longest() {
+        if (this.elements.isEmpty()) {
+            return null;
+        }
+
+        String longestStr = this.elements.get(0);
+        for (String str : elements) {
+            if (longestStr.length() < str.length()) {
+                longestStr = str;
+            }
+        }
+
+        return longestStr;
+    }
+
 }
